6bc45faba5370a2a815322a63d1be1c9ac5ad221
Priyanshu Joshi <joshipriyanshu575@gmail.com>
2026-03-14 19:26:46 +0530

    remove orgin

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+     origin: "*"
+}));
 app.use(express.json());
 app.use('/driver' , driverRoute)
 
